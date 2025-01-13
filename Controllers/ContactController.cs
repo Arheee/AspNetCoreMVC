1edca99327630fcf023786cb7c09bc83e6c74e5c
Arheee <justinewallet1@gmail.com>
2025-01-13 16:22:19 +0100

    [Exo1 et Exo2] Contact Afficher et Ajouter

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TpAspNetCoreMVC.Controllers
+{
+    public class ContactController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Votre liste de contacts ";
+            return View();
+        }
+
+      
+        public ActionResult Afficher()
+        {
+            ViewData["Contact"] = "Nicolas Durée";
+            return View();
+        }
+
+      
+        public ActionResult Ajouter()
+        {
+            ViewBag.Instruction = "Ici un Form pour ajouter un nouveau contact ";
+            return View();
+        }
+    }
+}
