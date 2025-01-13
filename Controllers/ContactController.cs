1602925943e54d0850313f7887ea2cd396345c73
Arheee <justinewallet1@gmail.com>
2025-01-13 16:50:40 +0100

    [Exo3] Debut en cours , pas finis

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,28 +1,46 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TpAspNetCoreMVC.Models;
 
 namespace TpAspNetCoreMVC.Controllers
 {
     public class ContactController : Controller
     {
+        static List<ContactModel> Contacts = new List<ContactModel>() {
+            new ContactModel { Id = 1, Nom = "Dupont", Prenom = "Jean", Email = "jean.dupont@example.com" },
+            new ContactModel { Id = 2, Nom = "Durand", Prenom = "Claire", Email = "claire.durand@example.com" },
+            new ContactModel { Id = 3, Nom = "Martin", Prenom = "Luc", Email = "luc.martin@example.com" }
+       };
        
         public ActionResult Index()
         {
-            ViewBag.Message = "Votre liste de contacts ";
+            ViewBag.Contacts = Contacts;
             return View();
         }
 
       
-        public ActionResult Afficher()
+        public ActionResult AfficherContact(int id)
         {
-            ViewData["Contact"] = "Nicolas Durée";
+            var contact = Contacts.FirstOrDefault(c => c.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            ViewData["Contact"] = contact;
             return View();
         }
 
-      
+
+        [HttpGet]
         public ActionResult Ajouter()
         {
-            ViewBag.Instruction = "Ici un Form pour ajouter un nouveau contact ";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Ajouter(ContactModel newContact)
+        {
             return View();
         }
     }
---
+++ Models/ContactModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TpAspNetCoreMVC.Models
+{
+    public class ContactModel
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+
+    }
+}
